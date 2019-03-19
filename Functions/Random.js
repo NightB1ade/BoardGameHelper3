ec82c82a88720f3d17dcd91b6bc7c84bca231461
NightB1ade <NightB1ade@users.noreply.github.com>
2019-03-19 22:06:43 +0000

    Random.js Code Clean-Up

--- Functions/Random.js
+++ Functions/Random.js
@@ -20,19 +20,19 @@ function getRandomIntegerExcludingOne(min,max,excluding) {
 
 
 function ShuffleArray(array) {
-	var i = 0
-	var j = 0
-	var temp = null
+	var i = 0;
+	var j = 0;
+	var temp = null;
 
 	for (
 		i = array.length - 1
 		; i > 0
 		; i -= 1
 	) {
-		j = Math.floor(Math.random() * (i + 1))
-		temp = array[i]
-		array[i] = array[j]
-		array[j] = temp
+		j = Math.floor(Math.random() * (i + 1));
+		temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
 	}
 
 	return array
